9aae6de46b0e070c5a423dddd84ca9c9be9889fc
Parshuram Dadas <dadasparshuram96@gmail.com>
2023-01-02 15:48:06 +0530

    second

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading1 = React.createElement("h1", { id: "first" }, "My First Heading");
+const heading2 = React.createElement(
+  "h1",
+  { id: "second" },
+  "My Second Heading"
+);
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
